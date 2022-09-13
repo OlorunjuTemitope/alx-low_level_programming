8690da558b5ef643af5dfddfccbdeb451725dd8b
OlorunjuTemitope <Temitope>
2022-09-13 09:31:18 -0700

    Nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,22 @@
-#include "_putchar"
+#include "main.h"
+	
+	/**
+	 * main - Entry point
+	 *
+	 * Return: Always returns 0
+	 *
+	 */
+	int main(void)
+	{
+	       _putchar('_');
+		_putchar('p');
+		_putchar('u');
+		_putchar('t');
+		_putchar('c');
+		_putchar('h');
+		_putchar('a');
+		_putchar('r');
+		_putchar('\n');
 
-/**
- * main - Entry point
- *
- * Return: Always returns 0
- *
- */
-int main(void)
-{
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
-	_putchar('\n');
 	return (0);
 }
