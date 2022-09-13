49395652faf68319e4559082ff42109305ade59c
OlorunjuTemitope <Temitope>
2022-09-13 15:32:16 -0700

    Ban

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,19 +1,17 @@
 #include "main.h"
-	/**
-	 * _islower - main function
-	 *
-	 * @c: character to compare
-	 *
-	 * Return: 1 if true, 0 if false
-	 *
-	 */
-	
-
-	int _islower(int c)
+/**
+  * _islower - main function
+  *
+  * @c: character to compare
+  *
+   * Return: 1 if true, 0 if false
+   *
+   */
+  int _islower(int c)
+  {
+	if (c >= 'a' && c <= 'z')
 	{
-		if (c >= 'a' && c <= 'z')
-		{
-			return (1);
-		}
-		return (0);
+		return (1);
 	}
+              return(1);
+  }
