447f9557cce69f03a52caee9c9a982039e6a5767
OlorunjuTemitope <Temitope>
2022-09-13 17:24:26 -0700

    Nigeria is a good place

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,22 @@
 #include "main.h"
 
 /**
-* main - print putchar 
-* Return: always 0
-*/
-
+ * main - Entry point
+ *
+ * Return: Always returns 0
+ *
+ */
 int main(void)
 {
-       _putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
 	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
-
 	return (0);
 }
