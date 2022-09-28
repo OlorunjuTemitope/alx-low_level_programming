de188e7cec3f73054afe52331b530832b4b54827
OlorunjuTemitope <Temitope>
2022-09-28 15:22:46 -0700

    Power is good

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,20 +1,17 @@
 #include "main.h"
 
 /**
- *
- * _puts_recursion - prints's a string followed by a new line
- * @s: string to be printed 
- *
- * Return: void 
+ * _puts_recursion - a function that prints a string, followed by a new line.
+ * @s: An input string to print
+ * Return: String
  */
 void _puts_recursion(char *s)
 {
-        if (*s == '\0)
-        {
-                _putchar('\n);
-        }
-        else
-        {
-                _putchar(s[0]);
-                _puts_recursion(s + 1);
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
 }
