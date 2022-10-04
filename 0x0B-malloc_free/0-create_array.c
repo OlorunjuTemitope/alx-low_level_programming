dab69e67b7aa3745e06b0a42e4c5d8e3e493a656
OlorunjuTemitope <Temitope>
2022-10-04 15:03:57 -0700

    Create an array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,4 +1,3 @@
-
 #include "main.h"
 
 /**
@@ -12,7 +11,6 @@ char *create_array(unsigned int size, char c)
         unsigned int i;
         char *s;
 
-
         if (size == 0)
                 return (NULL);
         s = (char *)malloc(size * sizeof(char));
@@ -20,7 +18,6 @@ char *create_array(unsigned int size, char c)
                 return (NULL);
         for (i = 0; i < size; i++)
                 s[i] = c;
-
 
         return (s);
 }
