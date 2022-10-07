91aa16d8e2deefa1c7e3a95f41f9fb1a50666453
OlorunjuTemitope <Temitope>
2022-10-07 10:23:31 -0700

    Create an array

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,15 +5,14 @@
  * malloc_checked - allocates memory using malloc
  * @b: memory to allocate
  *
- * Return: pointer to allocated memory or normal process termination if error    
+ * Return: pointer to allocated memory or normal process termination if error
  */
 void *malloc_checked(unsigned int b)
 {
-           
-        void *p;
-        
-        p = malloc(b);
-        if (p == NULL)
-                exit(98);
-        return (p);
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
 }
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,11 +1,11 @@
-#ifndef MAIN_H 
+#ifndef MAIN_H
 #define MAIN_H
 
 int _putchar(char c);
-void *malloc_checked(unsigned int n);
+void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
-void *realloc(void *ptr, unsigned int old-size, unsigned int new_size);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
-#endif /*MAIN_H/
+#endif /*MAIN_H*/
