923d36a78a220ae977a9ef0a6490b114b08d6571
OlorunjuTemitope <Temitope>
2022-10-10 15:40:43 -0700

    JESUS IS KING

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,24 +1,21 @@
-#ifndef STRTYP
-#define STRTYP 
+#ifndef DOG_H_FILE
+#define DOG_H_FILE
 
 /**
-  *struct dog - structure of a dog
-  *@name: name of dog
-  *@age: age of dog 
-  *@owner: owner of dog 
-  */
-struct dog
+ * struct dog - structure definition of a dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner
+ */
+typedef struct dog
 {
-        char *name;
-        float age;
-        char *owner;
-};
-
-typedef struct dog dog_t;
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d));
+void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
-#endif /*STRTYP*/
+#endif
